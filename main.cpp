558bf6808193cdecdf9853542908daaac824abf2
domso <domso94@googlemail.com>
2017-06-29 17:29:41 +0200

    added helper-functions and img-loading

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include "src/sample_set.h"
 
-int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
+int main(int argc, char** argv) {
+    auto v = data::sample_set::load<uint8_t>("../train-images.idx3-ubyte", "../train-labels.idx1-ubyte");
+
+    for (data::sample<uint8_t>& s : v) {
+
+    }
+
+    v[21835].store("out");
+    //v[0].print();
+    //v[0].store("test.pgm");
+
     return 0;
 }
---
+++ src/sample.h
@@ -0,0 +1,73 @@
+#ifndef data_sample_h
+#define data_sample_h
+
+#include <fstream>
+#include <vector>
+#include <stdint.h>
+
+namespace data {
+    template <typename imgT>
+    class sample {
+    public:
+
+        sample(const int id, const int width, const int height) : id_(id), width_(width), height_(height), internalData_(width_ * height_) {
+
+        }
+
+        imgT& operator [](const int index) {
+            return internalData_[index];
+        }
+
+        int getWidth() const {
+            return width_;
+        }
+
+        int getHeight() const {
+            return height_;
+        }
+
+        uint8_t getLabel() const {
+            return label_;
+        }
+
+        void setLabel(const uint8_t newLabel) {
+            label_ = newLabel;
+        }
+
+        void print() const {
+            std::cout << "sample-" << id_ << ": " << "(" << width_ << " x " << height_ << ") | " << (int) label_ << std::endl;
+        }
+
+        bool store(const std::string& filenamePrefix) {
+            std::ofstream file;
+            file.open(filenamePrefix + "_#" + std::to_string(id_) + "_" + std::to_string((int)label_) + ".pgm");
+
+            if (file.is_open()) {
+                //https://de.wikipedia.org/wiki/Portable_Anymap
+                file << "P5 ";
+                file << std::to_string(width_);
+                file << " ";
+                file << std::to_string(height_);
+                file << " 255 ";
+
+                for (imgT p : internalData_) {
+                    file << p;
+                }
+
+                return file.good();
+            }
+
+            return false;
+        }
+
+    private:
+        int id_;
+        int width_;
+        int height_;
+        uint8_t label_;
+        std::vector<imgT> internalData_;
+    };
+}
+
+#endif
+
