f08bb5c3abc15a79a1b554d64cbadf089607faec
domso <domso94@googlemail.com>
2017-07-16 23:48:59 +0200

    added posix barrier

--- src/barrier.hpp
+++ src/barrier.hpp
@@ -3,14 +3,23 @@
 #include <mutex>
 #include <condition_variable>
 #include <iostream>
+#include <pthread.h>
 
 class Barrier {
 public:
     explicit Barrier(const int n): lockCounter_(0), head_(n), size_(n) {
-
+    	pthread_barrier_init(&m_bar, 0, n);
     }
 
+    ~Barrier() {
+    	pthread_barrier_destroy(&m_bar);
+    }
+
+    Barrier(const Barrier& b) = delete;
+
     void wait() {
+    	pthread_barrier_wait(&m_bar);
+    	return;
         std::unique_lock<std::mutex> ul(mutex);
 
         // get current ticket
@@ -24,15 +33,17 @@ public:
             lockCounter_ = 0;
             // invalidates all current tickets
             head_ += size_;
+
+            // notify all other waiting threads
+            cond.notify_all();
+
+            return;
         }
 
         // wait until current ticket is invalid
         while (ticket == head_) {
             cond.wait(ul);
         }
-
-        // notify all other waiting threads
-        cond.notify_all();
     }
 
 private:
@@ -40,6 +51,7 @@ private:
     int head_;
     int size_;
 
+    pthread_barrier_t m_bar;
     std::mutex mutex;
     std::condition_variable cond;
 };
