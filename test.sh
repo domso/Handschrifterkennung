a10b6041bd721b67647266d0733ba639c49dae51
domso <domso94@googlemail.com>
2017-07-23 16:09:41 +0200

    added test-script

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+if [ $# -ne 2 ];
+then
+	echo "Require 2 parameters";
+	exit 0;
+fi
+
+for file in $(find tests/$1 -type f)
+do
+	echo $file >> $2;
+	./Release/handschrifterkennung $file >> $2;
+done
